87fefbf09043bd76fd98080e3d1b0cb084125599
chile122 <84024559+chile122@users.noreply.github.com>
2023-10-23 22:03:03 +0700

    Update driverFactory.java

--- src/test/java/driver/driverFactory.java
+++ src/test/java/driver/driverFactory.java
@@ -1,17 +1,14 @@
 package driver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class driverFactory {
     public static WebDriver getChromeDriver() {
-        String currentProjectLocation = System.getProperty("user.dir");
-        String chromeDriverRelativePath = "/src/test/resources/drivers/chromedriver.exe";
-        String chromeDriverLocation = currentProjectLocation.concat(chromeDriverRelativePath);
-        System.setProperty("webdriver.chrome.driver", chromeDriverLocation);
+        WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        //return new ChromeDriver();
         return driver;
     }
 }
